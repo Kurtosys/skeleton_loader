657e92b4130a0687f00dee4c34453420371da626
Blaine <blaine.lingard@kurtosys.com>
2019-07-09 10:53:15 +0200

    styled

---
+++ src/components/app/App.style.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const AppWrapper = styled.section`
+    background-color: red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
---
+++ src/components/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AppWrapper } from './App.style';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <AppWrapper>
+        <h1>home page</h1>
+      </AppWrapper>
+      </header>
+    </div>
+  );
+}
+
+export default App;
